f002770d9de02d5b1570e1d02f979ecd6484db0b
harsh11-code <harshpanchal1104@gmail.com>
2024-09-25 03:36:21 +0000

    datatypes

---
+++ harsh/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //teat all JS code as never version
+
+// alert ( 3 + 3 ) // we are using nodejs , not browser
+
+console.log(3
+
+    +3) // cod readability should  be high
+
+console.log("HARSH")
+
+let name = "HARSH"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to powe 53
+// bigint 
+//string => ""
+//Boolean => true/false
+//null => standalone value
+// undefind =>
+// symbol => unique
+
+//object
+
+console.log(typeof "HARSH")
+console.log(typeof undefined); //undefind
+console.log(typeof null); // object
---
+++ harsh/variable.js
@@ -0,0 +1,11 @@
+const accountID = 123456787
+let accountEmail = "harshpanchal233@gmail.com"
+var accountPassword = "1234"
+accountCity = "Surat"
+
+accountEmail = "abcd233@.com"
+accountPassword = "987654"
+accountCity = "Vesu"
+console.log(accountID0);
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
